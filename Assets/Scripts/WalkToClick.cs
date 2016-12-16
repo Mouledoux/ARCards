5d95aac43e691095ccaed0da6434dbaba60e47fa
Eric Mouledoux <EricMouledoux@gmail.com>
2016-12-16 12:42:22 -0600

    New script
    Robot color also set to green

---
+++ Assets/Scripts/WalkToClick.cs
@@ -0,0 +1,36 @@
+﻿/*  Eric Mouledoux 
+ *  Eric@tantrumlab.com
+ *  
+ *  
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Will Move whatever object has this component to the last position clicked in space
+/// </summary>
+public class WalkToClick : MonoBehaviour
+{
+    /// <summary>
+    /// Speed at which the entity moves at
+    /// </summary>
+    [SerializeField] private float mSpeed;
+
+    /// <summary>
+    /// How far the entity can be from the object before stopping
+    /// </summary>
+    [SerializeField] private float mStoppingDistance;
+
+    /// <summary>
+    /// Posistion in 3d space of the last "click"
+    /// </summary>
+    private Vector3 mTargetPos;
+
+
+	void Update ()
+    {
+
+	}
+}
