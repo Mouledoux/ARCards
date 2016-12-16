d3cde54f047ebfb857891f29bbc40f769605f107
Eric Mouledoux <EricMouledoux@gmail.com>
2016-12-16 16:35:41 -0600

    Correct angle, but inverted Y
    Y rotation needs to be inverted 180deg

--- Assets/Scripts/WalkToClick.cs
+++ Assets/Scripts/WalkToClick.cs
@@ -31,6 +31,43 @@ public class WalkToClick : MonoBehaviour
 
 	void Update ()
     {
+        if(Input.GetMouseButton(0))
+        {
+            Vector3 targetPos = Vector3.zero;
+            targetPos.y = transform.position.y;
 
+            Camera cam = Camera.main;               // Reference to main camera
+            Vector3 mousePos = Input.mousePosition; // Mouse posistoin at the time of clicking
+
+            Vector3 screenCenter = Vector3.zero;    // Finding the center of the screen
+            screenCenter.x = Screen.width / 2;      //
+            screenCenter.y = Screen.height / 2;     //
+
+            mousePos = mousePos - screenCenter;     // Gets the click posistion relative to the center of the screens
+
+            Vector3 clickAngle = Vector3.zero;          // Angle of the clicked posistion
+            clickAngle.y = mousePos.y / screenCenter.y;
+            clickAngle.x = mousePos.x / screenCenter.x;
+
+            clickAngle.y *= (cam.fieldOfView * ((float)Screen.height / (float)Screen.width));
+            clickAngle.x *= (cam.fieldOfView);
+
+            Vector3 angleToClick = Vector3.zero;                                // Angle of the click to the object
+            angleToClick = cam.transform.eulerAngles - transform.eulerAngles;   // relative to cameras rotation to the object
+            angleToClick = angleToClick + clickAngle;                           // 
+
+            float camHeight = 0.0f;                                         // Cameras height from the object
+            camHeight = cam.transform.position.y - transform.position.y;    //
+
+            print(clickAngle);
+            clickAngle.x += cam.transform.position.x;
+            clickAngle.y += cam.transform.position.y;
+            clickAngle.z *= -1;
+
+            clickAngle += cam.transform.eulerAngles;
+
+            Debug.DrawRay(cam.transform.position, clickAngle * 10, Color.red);
+        }
 	}
+
 }
